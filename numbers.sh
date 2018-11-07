43a834f2ed269ba212bb3836b3abf6510eddd80b
Alyssa Roach <aroach@chapman.edu>
2018-11-07 11:03:41 -0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,15 @@
+# numbers.sh
+# Alyssa Roach
+echo "Enter a positive number: "
+read NUMBER
+N=1
+while [ $N -le $NUMBER ]
+do
+	if [ $((N%2)) -eq 0 ]
+	then
+		echo "$N Even"
+	else
+		echo "$N Odd"
+	fi
+	N=$[$N + 1]
+done
